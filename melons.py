ef4c7e7c4a4b2ffcab75308a3129006641bc3610
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-17 18:10:41 -0800

    Calculate all melon type costs

---
+++ melons.py
@@ -0,0 +1,63 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder:
+
+    def __init__(self, species, qty, order_type, tax):
+        """An abstract base class that other Melon Orders inherit from."""
+        self.species = species
+        self.qty = qty
+        self.order_type = order_type
+        self.tax = tax
+        self.shipped = False
+        
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        base_price = 5
+        if self.species == "Christmas":
+            base_price *= 1.5
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            total += 10 
+
+        return total   
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    def __init__(self, species, qty):
+        super().__init__(species, qty, "domestic", 0.08)
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty, "international", 0.17)
+        """Initialize melon order attributes."""
+        self.country_code = country_code
+        
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty, "domestic", 0)
+        self.passed_inspection = False
+
+    def mark_inspection(self):
+        self.passed_inspection = True
+
+#####################################################################
+
+
+
